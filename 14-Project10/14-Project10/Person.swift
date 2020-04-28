35090506e2b6eae25fdb2628e2cbdc0a4d72f474
Arjun-dureja <arjun.dureja1000@gmail.com>
2020-04-28 16:22:12 -0400

    Finished project 10

---
+++ 14-Project10/14-Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  14-Project10
+//
+//  Created by Arjun Dureja on 2020-04-28.
+//  Copyright © 2020 Arjun Dureja. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
