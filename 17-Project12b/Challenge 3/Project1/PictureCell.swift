eb43e66ecb6ef0f5ff35c0501a3037696c271265
Arjun-dureja <arjun.dureja1000@gmail.com>
2020-04-29 01:18:49 -0400

    Finished project 12

---
+++ 17-Project12b/Challenge 3/Project1/PictureCell.swift
@@ -0,0 +1,14 @@
+//
+//  PictureCell.swift
+//  Project1
+//
+//  Created by Arjun Dureja on 2020-04-28.
+//  Copyright © 2020 Arjun Dureja. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    @IBOutlet var imageVIew: UIImageView!
+    @IBOutlet var name: UILabel!
+}
