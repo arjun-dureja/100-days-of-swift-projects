604491fa0069958612245fd612bfc582b02813c9
Arjun-dureja <arjun.dureja1000@gmail.com>
2020-04-29 21:30:46 -0400

    Finished milestone project 10-12

---
+++ 18-Projects10-12-Milestone/18-Projects10-12-Milestone/PictureCell.swift
@@ -0,0 +1,15 @@
+//
+//  PictureCell.swift
+//  18-Projects10-12-Milestone
+//
+//  Created by Arjun Dureja on 2020-04-29.
+//  Copyright © 2020 Arjun Dureja. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+    @IBOutlet var caption: UILabel!
+    @IBOutlet var picture: UIImageView!
+    
+}
